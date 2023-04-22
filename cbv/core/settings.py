4619438d8394e2f741821cfc83347f7cdbdf8150
Pavel Vavilin <shtartora@gmail.com>
2023-04-22 20:30:41 +0500

    ver 2.0

--- cbv/core/settings.py
+++ cbv/core/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / ".." / 'db.sqlite3',
     }
 }
 
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/auth/login/"
--- fbv/core/urls.py
+++ fbv/core/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('todo.urls')),
     path('admin/', admin.site.urls),
+    path("auth/", include("django.contrib.auth.urls")),
 ]
